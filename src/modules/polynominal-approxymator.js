7fc9f64747a8297404b86e722d4c3cba03b69db2
Mateusz Kulesza <mkulesza@gormanet.pl>
2014-09-25 21:34:37 +0200

    Polynomial Approxymator - fixed all outstanding unit tests

--- src/modules/polynominal-approxymator.js
+++ src/modules/polynominal-approxymator.js
@@ -4,7 +4,85 @@
 (function(Polynomial){
 "use strict";
 
-    Polynomial.Term = function(){};
+
+    Polynomial.Term = function(numerator,denominator){
+        if('undefined' === typeof denominator){
+            denominator = 1;
+        }
+        
+        this.getNumerator = function(){
+            return numerator;
+        };
+        
+        this.getDenominator = function(){
+            return denominator;
+        };
+        
+        // make chainable
+        return this;        
+    };
     
+    Polynomial.Term.prototype = {
+    
+        getValue: function(){
+            return parseFloat(this.getNumerator() / this.getDenominator());
+        },
+        
+        clone: function(){
+            return new Polynomial.Term(this.getNumerator(), this.getDenominator());
+        },
+        
+        multiply: function(value){
+            return new Polynomial.Term( this.getNumerator() * value, this.getDenominator() ); 
+        },
+        
+        divide: function(value){
+            return new Polynomial.Term( this.getNumerator(), this.getDenominator() * value ); 
+        }
+    
+    };
+    
+    // make auto-coercible to Number
+    Polynomial.Term.prototype.valueOf = Polynomial.Term.prototype.getValue;
+    
+    /*
+     *  Iterative Approxymator of Polynomials
+     *  - given number of terms generates serie of polynomial terms with given 'currentTerm' callback
+     *  - calculates sum of those terms to get polynomial approxymation of a math constant. 
+     */
+    Polynomial.IterativeApproxymator = function(name, termGenerator, firstTerm, parameter){
+        this.name = name;
+        this.generator = termGenerator;
+        this.firstTerm = firstTerm;
+        this.parameter = parameter;
+    };
+    Polynomial.IterativeApproxymator.prototype = {
+        getName: function(){
+            return this.name;
+        },
+        
+        calculate: function(numberOfTerms){
+            this.terms = [this.firstTerm];
+            
+            for(var index = 1; index < numberOfTerms; index++){
+                this.terms.push( this.generator( this.terms[index-1], index, this.parameter) );
+            }
+            
+            return this;
+        },
+        
+        // Sums all the terms to get approxymation
+        // TODO: support sum, mean, and custom approx algorithms
+        getApproxymation: function(){
+            return this.terms.reduce(function(prev, current, index, terms){            
+                return prev + current;
+            });
+        },
+        
+        getTerms: function(){
+            return this.terms;
+        }
+    };
 
 })(window.Polynomial || (window.Polynomial = {}));
+
--- test/test-polynominal-approxymator.js
+++ test/test-polynominal-approxymator.js
@@ -6,42 +6,44 @@ describe('Polynomial Module',function(){
      */
     describe('"Term" value-object implementation',function(){
     
-        var term_half,term_two;
+        var Polynomial = window.Polynomial, term_half,term_two;
+
         beforeEach(function(){
 	        term_half = new Polynomial.Term(1,2);
 	        term_two = new Polynomial.Term(2,1);
         });
 	    
-	    it('should create Term objects with numerator and denominator as arguments',function(){ 
-	        expect(term_half).toEqual(0.5);
-	        expect(term_two).toEqual(0.5);
+	    it('should create Term objects with numerator and optional denominator as arguments',function(){ 
+	        expect(new Polynomial.Term(2.5).getValue()).toEqual(2.5);
+	        expect(term_half.getValue()).toEqual(0.5);
+	        expect(term_two.getValue()).toEqual(2.0);
 	    });
 	    
 	    it('should be immutable, and return clones on operations',function(){ 
 	        var term_clone = term_half.clone();
 	        
 	        expect(term_half).not.toBe(term_clone);
-	        expect(term_half).toEqual(term_clone);
+	        expect(term_half.getValue()).toEqual(term_clone.getValue());
 	    });
 	    
 	    it('should multiply Term by another Term',function(){	        
-	        expect(term_half.multiply(term_two)).toEqual(1);	        
+	        expect(term_half.multiply(term_two).getValue()).toEqual(1);	        
 	    });
 	    
 	    it('should divide Term by another Term',function(){ 	        
-	        expect(term_half.divide(term_two)).toEqual(0.25);	        
+	        expect(term_half.divide(term_two).getValue()).toEqual(0.25);	        
 	    });
 	    
 	    it('should multiply Term by scalar value',function(){ 	        
-	        expect(term_half.multiply(2)).toEqual(1);	        
+	        expect(term_half.multiply(2).getValue()).toEqual(1);	        
 	    });
 	    
 	    it('should divide Term by scalar value',function(){ 	        
-	        expect(term_half.divide(2)).toEqual(0.25);	        
+	        expect(term_half.divide(2).getValue()).toEqual(0.25);	        
 	    });
 	    
 	    it('should be coercible to Number',function(){ 	        
-	        expect(term_half).toEqual(0.5);	        
+	        expect(term_half.getValue()).toEqual(0.5);	        
 	    });
 	});
 	
@@ -50,20 +52,20 @@ describe('Polynomial Module',function(){
 	 */
 	describe('Iterative Approxymation Module',function(){
 	
-	    var factorials_approx;
+	    var Polynomial = window.Polynomial, factorials_approx;
 	    beforeEach(function(){
 	        // sample easily predictable approxymator that sums factorials
-	        factorials_approx = new Polynominal.IterativeApproxymator("test-approxymator", 
+	        factorials_approx = new Polynomial.IterativeApproxymator("test-approxymator", 
 	            function(currentTerm, index, parameter){
                     return currentTerm.multiply(index+1);
-                }, new Polynominal.Term(1,1), 1);
+                }, new Polynomial.Term(1,1), 1);
 	        });
 	
 	    it('should create approxymator objects taking name, term function, optional first term and optional parameter ',function(){
             expect(factorials_approx.getName()).toBe("test-approxymator");
             
             // Test sample factorial 5!
-            expect(factorials_approx.calculate(5).getApproximation()).toEqual(  
+            expect(factorials_approx.calculate(5).getApproxymation()).toEqual(  
                 1 +
                 1*2 +
                 1*2*3 +
@@ -72,9 +74,11 @@ describe('Polynomial Module',function(){
             );
 	    });
 	
-        it('should generate given number of polynominal terms and return their values as an array',function(){
+        it('should generate given number of Polynomial terms and return their values as an array',function(){
             // Test sample factorial 5!
-            expect(factorials_approx.calculate(5).getTerms()).toEqual([
+            expect(factorials_approx.calculate(5).getTerms().map(function(t){
+                return t.getValue();
+            })).toEqual([
                 1,
                 1*2,
                 1*2*3,
